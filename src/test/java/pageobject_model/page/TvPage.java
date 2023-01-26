5494903faa0dbee9ea7c8dc958ceba0b78fe3598
Sashka88 <sasharyabceva@gmail.com>
2023-01-26 19:15:01 +0300

    Refactoring

--- src/test/java/pageobject_model/page/TvPage.java
+++ src/test/java/pageobject_model/page/TvPage.java
@@ -3,16 +3,20 @@ package pageobject_model.page;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
 public class TvPage extends AbstractPage {
-    JavascriptExecutor js = (JavascriptExecutor)driver;
+//    Actions builder = new Actions(driver);
+    JavascriptExecutor js = (JavascriptExecutor) driver;
 
-    @FindBy(xpath = "//input[@value='samsung' and (@type='checkbox')]")
-    private List<WebElement> SearchSamsungChbx;
+
+    @FindBy(xpath = "//ul//input[@type='checkbox' and @value='samsung']")
+    private WebElement searchSamsungChbx;
 
     //@FindBy(xpath = "//li[2]//input[@value='samsung']")
     //private WebElement SearchPriceInput;
@@ -23,9 +27,13 @@ public class TvPage extends AbstractPage {
 
     public TvPage selectTvMaker()  {
         new WebDriverWait(driver, WAIT_TIMEOUT_SECONDS)
-                .until(condition -> SearchSamsungChbx.get(0).isDisplayed());
-        //SearchSamsungChbx.get(0).click();
-        js.executeScript("arguments[0].click();", SearchSamsungChbx);
+                .until(condition -> searchSamsungChbx.isDisplayed());
+        js.executeScript("arguments[0].click();", searchSamsungChbx);
+//        Action seriesOfActions = builder
+//                .moveToElement(SearchSamsungChbx)
+//                .click()
+//                .build();
+//        seriesOfActions.perform();
         return this;
     }
 
