8b59896b60892cd16060a506c038e423f94beb96
Sashka88 <sasharyabceva@gmail.com>
2023-03-03 17:56:54 +0300

    Refactoring WebDriver test

--- src/test/java/pageobject_model/page/Browser.java
+++ src/test/java/pageobject_model/page/Browser.java
@@ -1,2 +1,18 @@
-package pageobject_model.page;public class Browser {
+package pageobject_model.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Browser extends AbstractPage {
+    public WebDriver driver;
+
+    public Browser(WebDriver driver) {
+        super(driver);
+    }
+
+    public static void openOnliner() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.onliner.by/");
+    }
 }
